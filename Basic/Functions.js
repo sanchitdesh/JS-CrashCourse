e7447d31cfde8d1a478f144d3d45a1d7fec4c155
Sanchit <Sanchit@gmail.com>
2024-05-18 17:41:21 +0530

    Functions in JS

---
+++ Basic/Functions.js
@@ -0,0 +1,46 @@
+//Agenda:
+// WHat are functions?
+// Different between?
+// ES6?
+
+// Functions:
+// Function Declaration
+// Function Expression
+
+// Function Declaration
+
+function typeName(a, b, c, d) {
+  //Code to be executed
+}
+
+// Examples:
+
+myData("MyWAY", 27);
+
+function myData(name, age) {
+  //   console.log("My Name is", name);
+  //   console.log("My Age is:", age);
+}
+
+//Function Expression:
+
+//Regular EXpression
+
+const myDataNew = function (rollno) {
+  console.log("My Roll no is", rollno);
+};
+
+myDataNew(24);
+
+//=======================
+
+//ES6 - Have introduced short Hand Function (Arrow Function) to make simple to the
+//regular function expression
+
+const data = (fName, lName) => {
+  //   console.log(`My FName is ${fName} and LName is ${lName}`);
+};
+
+data("Java", "Script");
+
+//Argumnets(Values) corresponds to the parameter
